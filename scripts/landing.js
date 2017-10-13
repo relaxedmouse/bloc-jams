65f639bb7cd91184b5bc735141e6b5c1772e681b
jesse versteeg <jessegreyson@gmail.com>
2017-10-13 17:48:13 -0600

    fix

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,15 @@
-var pointsArray = document.getElementsByClassName('point');
+var animatePoints = function() {
+  var revealPoint = function() {
+          // #7
+          $(this).css({
+              opacity: 1,
+              transform: 'scaleX(1) translateY(0)'
+          });
+        }
+        $.each($('.point'), revealPoint);
+      };
 
-var revealPoint = function(point) {
-  point.style.opacity = 1;
-  point.style.transform = "scaleX(1) translateY(0)";
-  point.style.msTransform = "scaleX(1) translateY(0)";
-  point.style.WebkitTransform = "scaleX(1) translateY(0)";
-};
-
-var animatePoints = function(points) {
-
-  forEach(points, revealPoint);
-
-};
-
-window.onload = function() {
+$(window).load(function() {
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
   window.addEventListener('scroll', function(event) {
