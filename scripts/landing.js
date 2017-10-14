26e2d928d8c5581d8486465c100c867fc50f5d45
jesse versteeg <jessegreyson@gmail.com>
2017-10-13 18:04:37 -0600

    fix
    fix

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,19 @@
 var animatePoints = function() {
   var revealPoint = function() {
-          // #7
-          $(this).css({
-              opacity: 1,
-              transform: 'scaleX(1) translateY(0)'
-          });
-        }
-        $.each($('.point'), revealPoint);
-      };
+      // #7
+      $(this).css({
+          opacity: 1,
+          transform: 'scaleX(1) translateY(0)'
+      });
+};
+ $.each($('.point'), revealPoint);
+};
 
 $(window).load(function() {
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-  window.addEventListener('scroll', function(event) {
-    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+   $(window).scroll(function(event) {
+    if ($(window).height() > 950) {
+    animatePoints();
     }
   });
-}
+ });
