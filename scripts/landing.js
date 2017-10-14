ed0d00ccd4303ec9fdcb38ad4af4edbb263df83c
jesse versteeg <jessegreyson@gmail.com>
2017-10-13 18:37:41 -0600

    finished checkpoint 16

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,19 +1,25 @@
 var animatePoints = function() {
   var revealPoint = function() {
-      // #7
-      $(this).css({
-          opacity: 1,
-          transform: 'scaleX(1) translateY(0)'
-      });
-};
- $.each($('.point'), revealPoint);
+    // #7
+    $(this).css({
+      opacity: 1,
+      transform: 'scaleX(1) translateY(0)'
+    });
+  };
+
+  $.each($('.point'), revealPoint);
 };
 
 $(window).load(function() {
-  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
-   $(window).scroll(function(event) {
-    if ($(window).height() > 950) {
-    animatePoints();
+      if ($(window).height() > 950) {
+        animatePoints();
+      }
+
+      var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+      $(window).scroll(function(event) {
+        if ($(window).scrollTop() >= scrollDistance) {
+          animatePoints();
+        }
+      });
     }
-  });
- });
